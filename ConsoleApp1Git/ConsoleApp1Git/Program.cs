db630ca3c43f6be6c82402126ec3c4344fc705d3
imgautamthakur <imgautamthakur@gmail.com>
2023-11-22 17:02:27 +0530

    initial commit

---
+++ ConsoleApp1Git/ConsoleApp1Git/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1Git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a =10, b =10;
+            Console.WriteLine(a + b);
+            Console.Read();
+        }
+    }
+}
