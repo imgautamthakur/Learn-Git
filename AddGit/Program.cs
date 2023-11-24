9d5842daeb2d89808f8a7b7d87415334923678bc
imgautamthakur <imgautamthakur@gmail.com>
2023-11-24 12:13:47 +0530

    Add project files.

---
+++ AddGit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=10; int b=20;
+            Console.WriteLine("Sum of two no:" + (a + b));
+            Console.Read();
+        }
+    }
+}
